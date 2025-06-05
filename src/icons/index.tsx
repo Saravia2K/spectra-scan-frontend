bd7203bfa585239875baf8a72a0469d99c6699da
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-06-05 07:46:11 -0600

    Dashboard layout for logged users

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,5 +1,13 @@
-import ChevronLeftIcon from "./chevron-left.svg";
-import EyeCloseIcon from "./eye-close.svg";
 import EyeIcon from "./eye.svg";
+import EyeCloseIcon from "./eye-close.svg";
+import ChevronLeftIcon from "./chevron-left.svg";
+import ChevronDownIcon from "./chevron-down.svg";
+import HorizontalDotsIcon from "./horizontal-dots.svg";
 
-export { ChevronLeftIcon, EyeCloseIcon, EyeIcon };
+export {
+  EyeIcon,
+  EyeCloseIcon,
+  ChevronLeftIcon,
+  ChevronDownIcon,
+  HorizontalDotsIcon,
+};
